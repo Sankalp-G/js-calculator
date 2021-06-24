35fe35cd915f78d11981e9f9144fe9313fcba3ff
Sankalp-G <sankalp.gawali@gmail.com>
2021-06-24 20:13:56 +0530

    created logic for all numericals and period buttons

--- scripts.js
+++ scripts.js
@@ -1,4 +1,10 @@
-//basic math function declarations
+////query selectors
+const buttonsGrid = document.querySelector('#buttonsContainer')
+const calcButtons = buttonsGrid.childNodes
+const mainDisplay = document.querySelector('#mainDisplay')
+const subDisplay = document.querySelector('#subDisplay')
+
+////basic math function declarations
 function add(num1, num2) {
     return num1 + num2
 }
@@ -20,10 +26,9 @@ function operate(operator, num1, num2) {
     return this[operator](num1, num2)
 }
 
-//logic to construct calculator buttons
-const buttonsGrid = document.querySelector('#buttonsContainer')
+////logic to construct calculator buttons
 let button = document.createElement('button')
-let buttonsTemplate = ['CE', 'C', '←', '/', 7, 8, 9, '*', 4, 5, 6, '-', 1, 2, 3, '+', 0, '.', '=']
+let buttonsTemplate = ['AC', 'CE', '←', '/', 7, 8, 9, '*', 4, 5, 6, '-', 1, 2, 3, '+', 0, '.', '=']
 for (i = 0; i < 19; i++) {
     let clonedButton = button.cloneNode()
     clonedButton.textContent = buttonsTemplate[i];
@@ -33,3 +38,54 @@ for (i = 0; i < 19; i++) {
     if(['/', '-', '+', '*'].indexOf(buttonsTemplate[i]) + 1){clonedButton.setAttribute('style', 'background-color: #e6f1ff;')}
     buttonsGrid.appendChild(clonedButton)
 }
+
+////event listeners
+//numerical logic
+let numericals = ['1','2','3','4','5','6','7','8','9','0']
+calcButtons.forEach(button => {
+    if (numericals.indexOf(button.textContent) + 1) {
+        button.addEventListener('click', () => {
+            mainDisplay.textContent += button.textContent;
+        })
+    }
+})
+
+//period logic
+calcButtons.forEach(button => {
+    if (button.textContent == '.') {
+        button.addEventListener('click', () => {
+            if (mainDisplay.textContent == '') { mainDisplay.textContent += '0.' }
+            else if ((['/', '*', '-', '+'].indexOf(mainDisplay.textContent.split(' ')[mainDisplay.textContent.split(' ').length - 2]) + 1) == 0
+                && mainDisplay.textContent.split('').indexOf('.') == -1) { mainDisplay.textContent += '.' }
+            else {
+                let split = mainDisplay.textContent.split(' ')
+                if (split[split.length - 1] == '') { mainDisplay.textContent += '0.' }
+                else if (split[split.length - 1].indexOf('.') == -1) { mainDisplay.textContent += '.' }
+            }
+        })
+    }
+})
+
+//clear buttons logic
+calcButtons.forEach(button => {
+    if(button.textContent == 'AC'){
+        button.addEventListener('click', () => {
+            mainDisplay.textContent = '';
+            subDisplay.textContent = '';
+        })
+    }
+    if(button.textContent == 'CE'){
+        button.addEventListener('click', () => {
+            mainDisplay.textContent = '';
+        })
+    }
+})
+
+//partial operator logic
+calcButtons.forEach(button => {
+    if(['/', '*', '-', '+'].indexOf(button.textContent) + 1){
+        button.addEventListener('click', () => {
+                mainDisplay.textContent += ' '+button.textContent+' ';
+            })
+        }
+})
